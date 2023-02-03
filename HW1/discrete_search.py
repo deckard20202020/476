dd126ff30d4d378d39b8ac3f9e4e8d607cd79a7d
Matt Sinnwell <mattdsinnwell@gmail.com>
2023-02-02 20:25:55 -0600

    getting started

--- HW1/discrete_search.py
+++ HW1/discrete_search.py
@@ -5,10 +5,21 @@ ALG_ASTAR = "astar"
 
 class StateSpace:
     """A base class to specify a state space X"""
+    """a list or set of goal states"""
+    """initial state is a tuple"""
+    """will we need a new class to represent a state?"""
+    """a set or list of states"""
+    """needs to be able to be searched quickly for contains method"""
+
+    def __init__(self, initial_state, goal_states, states):
+        self.initial_state = initial_state
+        self.goal_states = set(goal_states)
+        self.states = set(states)
 
     def __contains__(self, x):
         """Return whether the given state x is in the state space"""
-        raise NotImplementedError
+        return x in self.states
+        """raise NotImplementedError"""
 
     def get_distance_lower_bound(self, x1, x2):
         """Return the lower bound on the distance between the given states x1 and x2"""
@@ -17,6 +28,7 @@ class StateSpace:
 
 class ActionSpace:
     """A base class to specify an action space"""
+    """A set of all actions"""
 
     def __call__(self, x):
         """ Return the list of all the possible actions at the given state x"""
@@ -46,4 +58,5 @@ def fsearch(X, U, f, xI, XG, alg):
                states visited during the search and path is a path from xI to a state in XG
     """
     # TODO: Implement this function
+    """will I need a function generateQueue(algo) that generates the type of queue needed for each algo"""
     raise NotImplementedError
